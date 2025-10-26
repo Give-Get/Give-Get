eef28a481b6fa53d8bda030f07a832765dcd9d5b
Kas-Bruh <kasra.ghadimi6@gmail.com>
2025-10-26 02:44:04 -0400

    new json, and functions for data

--- backend/db.py
+++ backend/db.py
@@ -11,7 +11,7 @@ MONGO_URI = "mongodb+srv://praj:praj@give-and-get-data.undmkkl.mongodb.net/?appN
 client = MongoClient(MONGO_URI)
 db = client['HackPSU-User-Data']
 users = db.users
-orgs = db.orgs
+orgs = db.organizations
 app_stats = db.app_statistics 
 
 
@@ -23,60 +23,84 @@ def update_database(id:str, new_json:dict, type:str):
     id: id of user or org you want to change in users or orgs database
     type: is equal to either "users" or "organizations"
     """
-    return
+    collection = db.users if type == "users" else db.organizations
 
-def update_users(id:str, new_json:dict):
+    # Ensure the new JSON includes the same _id
+    if "_id" not in new_json:
+        new_json["_id"] = id
+
+    result = collection.replace_one({"_id": id}, new_json, upsert=True)
+    return f"Updated {result.modified_count} document(s)."
+
+
+def update_user(id:str, new_json:dict):
     """
     id: id of user you want to change in users database
     things_changed: dictionary with changes you wish to make in ket,value format
     """
-    update_database(id, new_json, "users") #change "new_json" from the new_json parameter when done with func
+    update_database(id, new_json, "users") 
     return
 
-def update_organizations(id:str, new_json:dict):
+def update_organization(id:str, new_json:dict):
     """
     id: id of org you want to change in orgs database
     things_changed: dictionary with changes you wish to make in ket,value format
     """
-    update_database(id, new_json, "organizations") #change "new_json" from the new_json parameter when done with func
+    update_database(id, new_json, "organizations")
     return
 
-def create_user(id):
-
-    pass
-
-def create_org(id):
+def create_user(new_json, id=None):
+    """
+    new_json: the json for the new user
+    id: will be a num if called by create_org()
+    """
+    if not id:
+        id = generate_id()
     
+    update_user(id, new_json)
+
     pass
+
+def create_organization(new_json):
+    """
+    """
+    id = generate_id()
+
+    update_organization(id, new_json)
+
+    pass
+
+def generate_id():
+    
+    return
 
 def add_user_and_org(type, object_json):
     
     return
 
-
-
-def collect_from_database(id:str, type:str):
-    """
-    id: id of user or org you want to change in users or orgs database
-    type: is equal to either "users" or "organizations"
-    """
-    return
-
-def colleect_users(id:str):
+def collect_user(id:str):
     """
     id: id of user you want to in users database
     things_changed: dictionary with changes you wish to make in ket,value format
     """
-    collect_from_database(id, "users") #change "new_json" from the new_json parameter when done with func
-    return
+    collection = db.users
+    user = collection.find_one({"_id": id})
+    if user:
+        return user
+    else:
+        raise ValueError(f"User with id {id} not found")
 
-def collect_organizations(id:str):
+def collect_organization(id:str):
     """
     id: id of org you want to change in orgs database
     things_changed: dictionary with changes you wish to make in ket,value format
     """
-    collect_from_database(id, "organizations") #change "new_json" from the new_json parameter when done with func
-    return
+    collection = db.organizations
+    organization = collection.find_one({"_id": id})
+    if organization:
+        return organization
+    else:
+        raise ValueError(f"User with id {id} not found")
 
 def get_orgs_within_radius(location, radius, org_type = None):
     """
@@ -85,13 +109,13 @@ def get_orgs_within_radius(location, radius, org_type = None):
     org_type: type of organization you want to query: none by default
     """
     lat, lon = location['lat'], location['long'] 
-    all_orgs_dict = list(db.organizations.find())[0]
+    all_orgs_dict = db.organizations.find()
 
     orgs_within_radius = []
-    for org_id, org_dict in all_orgs_dict.items:
-        org_lat, org_lon = get_org_coords(org_dict)
-        if calculate_distance(lat, lon, org_lat, org_lon) < radius and not None:
-            orgs_within_radius.append(org_dict)
+    for org in all_orgs_dict:
+        org_lat, org_lon = get_org_coords(org)
+        if calculate_distance(lat, lon, org_lat, org_lon) < radius:
+            orgs_within_radius.append(org)
 
     if not org_type:
         return orgs_within_radius
@@ -102,6 +126,7 @@ def get_orgs_within_radius(location, radius, org_type = None):
 def get_org_coords(org_dict):
     if not isinstance(org_dict,dict):
         return None
+    
     lat = org_dict['location']['lat']
     lon = org_dict['location']['lon']
     return lat, lon
